c3ce8649c2ecfc9245d965560927284010c56ed4
costarafa <rafaelacs.rc@gmail.com>
2021-11-02 13:39:48 -0300

    criacao do repositorio e interfaces

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [sabor, setSabor] = useState('');
+  const [preco, setPreco] = useState('');
+  const [tamanho, setTamanho] = useState('');
+
+  const cadastro = () => {
+    alert('funcionando');
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <StatusBar hidden />
+
+        <TextInput
+          placeholder= "Sabor da marmitinha" 
+          style={styles.textInput} 
+          onChangeText={text=>setSabor(text)}
+        />
+        <TextInput 
+            placeholder= "Preco da marmitinha"
+            style={styles.textInput}
+            onChangeText={text=>setPreco(text)}
+        />
+
+        <TextInput
+          placeholder= "Tamanho da marmitinha"
+          style={styles.textInput}
+          onChangeText={text=> setTamanho(text)}
+        />  
+
+        <TouchableOpacity style={styles.btnCadastro} onPress={() => cadastro()}>
+          <Text>CADASTRAR</Text>
+        </TouchableOpacity>        
     </View>
   );
 }
@@ -14,8 +43,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFDEAD',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+
+  textInput: {
+    width: '100%',
+    height: 40,
+    backgroundColor:'#DEB887',
+    borderRadius: 20,
+    paddingLeft: 10  
+  },
+
+  btnCadastro: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#A0522D',
+    borderRadius: 20,
+    justifyContent: center
+  }
 });
