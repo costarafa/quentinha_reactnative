6b40d746caf50fc126d7dee7ca9b33fc92dc61cd
costarafa <rafaelacs.rc@gmail.com>
2022-02-07 14:03:06 -0300

    imagens

--- src/pages/Detalhes.js
+++ src/pages/Detalhes.js
@@ -1,12 +1,24 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StylesSheet} from 'react-native';
+import {Feather, Ionicons} from '@expo/vector-icons';
+import {ScrollView} from 'react-native-gesture-handler';
+import Stars from 'react-native-stars';
+
+import SwiperComponent from '../componentes/Swiper';
 
 export default function Detalhes(){
     return (
-        <View>
-            <Text>
-                Detalhes
-            </Text>
+        <View style= {styles.container}>
+            <View style={styles.swiperContent}>
+                <SwiperComponent/>
+            </View>
+
+            <View style={styles.headerContent}>
+                <View style={{width: '65%'}}>
+                    <Text style={styles.marmitinha}> Marmitinha</Text>    
+                </View> 
+
+            </View>
         </View>
     );
 }
